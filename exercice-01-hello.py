c1102e396263ea0c4e62c86539d25741fec50703
myibe <myriamibed@gmail.com>
2022-02-28 16:02:37 +0100

    Création du repo
    Ajout des premiers chapitre de cours.
    Ajout des exos correspondants.

---
+++ exercice-01-hello.py
@@ -0,0 +1,15 @@
+# exercice-01-hello.py
+
+# exo 1.1
+# Affichez le message "programme démarré" en utilisant des doubles quotes
+
+# réponse 1.1
+print("programme démarré")
+
+# exo 1.2
+# Affichez le message "programme terminé" en utilisant des simples quotes
+
+# réponse 1.2
+print('programme terminé')
+
+
---
+++ variables.py
@@ -0,0 +1,53 @@
+from __future__ import barry_as_FLUFL
+from cgitb import text
+from operator import truediv
+from re import MULTILINE
+from xxlimited import foo
+
+
+nombre = 123
+print(nombre)
+
+nombre = 3.14
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "John \"Dead\" Doe"
+nickname = ' John O\'Connor'
+print(nickname)
+
+
+multiline_text = """foo
+bar
+baz"""
+print(multiline_text)
+
+
+
+
+# foo
+# bar
+# baz
+
+
+
+
+
