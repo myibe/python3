340f6a6061fcee66adda0f84f91fb7160feb21a1
myibe <myriamibed@gmail.com>
2022-03-02 11:58:16 +0100

    Mise à jour cours variables
    Exploration de conversions de type

--- variables.py
+++ variables.py
@@ -7,18 +7,25 @@ from xxlimited import foo
 
 nombre = 123
 print(nombre)
+print(type(nombre))
+
 
 nombre = 3.14
 print(nombre)
+print(type(nombre))
 
 nombre = "123"
 print(nombre)
+print(type(nombre))
+
 
 text = "lorem ipsum"
 print(text)
 
 is_day = True
 print(is_day)
+print(type(is_day))
+
 
 
 has_sugar = False
@@ -26,6 +33,8 @@ print(has_sugar)
 
 has_accepted_ula = None
 print(has_accepted_ula)
+print(type(has_accepted_ula))
+
 
 html_code = '<h1>Titre de mon blog</h1>'
 print(html_code)
@@ -41,7 +50,37 @@ baz"""
 print(multiline_text)
 
 
+nombre = "5"
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
 
+nombre = "2.71"
+print(nombre)
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var =
 
 # foo
 # bar
