b41205f80b27a4247e2b292d29f445ddd9541e3e
myibe <myriamibed@gmail.com>
2022-04-26 22:44:11 +0200

    boucles - terminé

---
+++ exercice-07-boucles.py
@@ -0,0 +1,251 @@
+# exercice-07-boucles.py
+
+import random
+
+# exo 7.1
+# en utilisant une boucle for, affichez les nombre de 0 à 99 inclus
+print("exo 7.1)")
+
+for counter in range (0, 100):
+    print('for python', counter)
+
+# réponse 7.1
+
+# exo 7.2
+# en utilisant une boucle for, affichez les nombre de 0 à 100 inclus
+for counter in range (0, 101):
+    print('for python', counter)
+
+# réponse 7.2
+
+# code 7.1
+# affectation d'un nombre aléatoire compris entre 1 et 10 inclus
+number = random.randint(1, 10)
+
+# exo 7.3
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est égal à 1
+
+# réponse 7.3
+
+print("exo 7.4")
+
+
+for i  in range (0, 101):
+    r = random.randint(1, 11)
+
+    if r == 1:
+        print(r)
+
+
+
+# exo 7.4
+# en utilisant une boucle for, on tire 50 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est plus petit ou égal à 5
+
+for i  in range (0, 51):
+    r = random.randint(1, 11)
+
+    if r >= 5:
+        print(r)
+
+
+# réponse 7.4
+
+# exo 7.5
+# en utilisant une boucle for, on tire 20 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est plus grand ou égal à 6
+
+# réponse 7.5
+
+for i  in range (0, 21):
+    r = random.randint(1, 11)
+
+    if r <= 6:
+        print(r)
+
+
+# exo 7.6
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est égal à 1 ou égal à 10
+
+# réponse 7.6
+for i  in range (0, 101):
+    r = random.randint(1, 11)
+
+    if r == 1 :
+        print(r)
+    elif r == 10:
+        print(r)
+
+
+# exo 7.7
+# en utilisant une boucle for, on tire 10 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est compris entre 3 et 8 inclus
+
+# réponse 7.7
+
+for i  in range (0, 10):
+    r = random.randint(1, 11)
+
+    if 3 <= r <= 8:
+        print(r)
+
+
+
+# exo 7.8
+# en utilisant une boucle for, on tire 50 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est égal à 7
+# affichez la variable `count`
+
+# réponse 7.8
+print("exo 7.8")
+count = 0
+
+for i in range (0,50):
+    r = random.randint(1,11)
+
+    if r == 7:
+        count = count +1
+
+print(count)
+
+# exo 7.9
+# en utilisant une boucle for, on tire 10 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est plus petit ou égal à 4
+# affichez la variable `count`
+
+# réponse 7.9
+
+for i in range (0,10):
+    r = random.randint(1,11)
+
+    if r <= 4:
+        count = count +1
+
+print(count)
+
+
+# exo 7.10
+# en utilisant une boucle for, on tire 33 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est plus grand ou égal à 7
+# affichez la variable `count`
+
+
+# réponse 7.10
+
+for i in range (0,33):
+    r = random.randint(1,11)
+
+    if r >= 7:
+        count = count +1
+
+print(count)
+
+# exo 7.11
+# en utilisant une boucle for, on tire 66 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est plus petit ou égal à 2, ou plus grand ou égal à 9
+# affichez la variable `count`
+
+# réponse 7.11
+
+print("exo 7.11)")
+for i in range (0,10):
+    r = random.randint(1,11)
+
+    if r >=2 | r <=9 :
+        count = count +1
+
+print(count)
+
+
+# exo 7.12
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# utilisez une variable compteur `count` pour compter le nombre de fois où `r` est compris entre 2 et 9 inclus
+# affichez la variable `count`
+
+# réponse 7.12
+
+for i in range (0,10):
+    r = random.randint(1,11)
+
+    if  2 >= r <= 9:
+        count = count +1
+
+print(count)
+
+
+# exo 7.13
+# en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 99 inclus
+
+# réponse 7.13
+
+print('exo7.13')
+
+for i  in range (0, 100):
+    if i % 2 == 0:
+        print(i)
+
+
+
+# exo 7.14
+# en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 100 inclus
+
+# réponse 7.14
+for i in range (0, 101,2):
+    print(i)
+    
+
+# exo 7.15
+# en utilisant une boucle for, affichez tous les nombres divisibles par 3, de 2 à 99 inclus
+
+# réponse 7.15
+for i in range (0, 99):
+    if i % 3 == 0:
+        print(i)
+    
+
+# code 7.2
+# pour calculer la puissance 2 d'un nombre, on peut le multiplier par lui-même
+number = random.randint(3, 10)
+print(number * number)
+# mais on peut aussi l'élever à la puissance 2 avec l'opérateur puissance `**`
+print(number ** 2)
+
+# exo 7.16
+# en utilisant une boucle for, affichez la puissance 2 des nombres de 0 à 99 inclus
+
+# réponse 7.16
+for i in range (0, 100):
+    print(i **2)
+
+# code 7.3
+# pour calculer la puissance 3 d'un nombre, on peut l'élever à la puissance 3 avec l'opérateur puissance `**`
+number = random.randint(3, 10)
+print(number ** 3)
+
+# exo 7.17
+# en utilisant une boucle for, affichez la puissance 3 des nombres de 1 à 100 inclus
+
+
+# réponse 7.17
+for i in range (0, 100):
+    print(i **3)
+
+# exo 7.18
+# dans une boucle while, on tire un nombre entier `r` au hasard entre 1 et 100 inclus
+# boucler jusqu'à ce que la valeur 100 soit tirée au hasard
+
+# for i in range(1,100):
+#     while True:
+#         n = random.randint(1, 5)
+
+#     #condition d'arret :
+#         if n not in numbers:
+#         # le nombre n'a pas encore été tiré au hasard
+#         # on peut sortir de la boucle
+#             break
+
+while r != 100 :
+    r= random.randint(1,100)
+    print(r)
